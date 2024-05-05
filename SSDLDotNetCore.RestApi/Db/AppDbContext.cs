a2eb793ee4e5ed9ae6b9284405c393dadb8e8ce3
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-05-05 12:47:37 +0700

    ASP.Net Core Web API EFCore CRUD

---
+++ SSDLDotNetCore.RestApi/Db/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSDLDotNetCore.RestApi.Db;
+using SSDLDotNetCore.ConsoleApp.Services;
+using SSDLDotNetCore.RestApi.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSDLDotNetCore.RestApi.Db
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
