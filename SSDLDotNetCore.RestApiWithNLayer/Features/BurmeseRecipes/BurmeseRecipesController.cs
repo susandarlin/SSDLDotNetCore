624d60e2f6ff80ff3a5451bcf31372a03a919c98
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-05-21 23:00:50 +0700

    BPI - Burmese Recipes

---
+++ SSDLDotNetCore.RestApiWithNLayer/Features/BurmeseRecipes/BurmeseRecipesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using static SSDLDotNetCore.RestApiWithNLayer.Features.LatHtaukBayDin.LatHtaukBayDinController;
+
+namespace SSDLDotNetCore.RestApiWithNLayer.Features.BurmeseRecipes
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BurmeseRecipesController : ControllerBase
+    {
+        public async Task<BurmeseRecipeList> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("BurmeseRecipes.json");
+            var arrayJson = jsonStr.ToArray();
+            var model = JsonConvert.DeserializeObject<BurmeseRecipeList>(jsonStr);
+            return model;
+        }
+        [HttpGet("BurmeseRecipeList")]
+        public async Task<IActionResult> BurmeseRecipeMenu()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.BurmeseRecipe);
+        }
+
+        [HttpGet("{guid}")]
+        public async Task<IActionResult> Detail(string guid)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.BurmeseRecipe.FirstOrDefault(x => x.Guid == guid));
+        }
+
+        public class BurmeseRecipeList
+        {
+            public BurmeseRecipe[] BurmeseRecipe { get; set; }
+            public Usertype[] UserTypes { get; set; }
+        }
+
+        public class BurmeseRecipe
+        {
+            public string Guid { get; set; }
+            public string Name { get; set; }
+            public string Ingredients { get; set; }
+            public string CookingInstructions { get; set; }
+            public string UserType { get; set; }
+        }
+
+        public class Usertype
+        {
+            public int UserId { get; set; }
+            public string UserCode { get; set; }
+            public string UserMMType { get; set; }
+            public string UserEngType { get; set; }
+        }
+
+
+    }
+}
