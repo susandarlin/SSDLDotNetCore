1f0120048026e7b5129539dd6648396bfd33939d
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-05-24 23:20:27 +0700

    Window Form - Hello World

---
+++ SSDLDotNetCore.WinFormsApp/Form1.cs
@@ -0,0 +1,15 @@
+namespace SSDLDotNetCore.WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
+    }
+}
