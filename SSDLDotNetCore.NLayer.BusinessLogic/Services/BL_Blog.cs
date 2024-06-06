27c12a870dbe250da598fcbf99333a756ebf02a7
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-06-06 23:02:12 +0700

    NLayer Architecture Console App

---
+++ SSDLDotNetCore.NLayer.BusinessLogic/Services/BL_Blog.cs
@@ -0,0 +1,50 @@
+﻿using SSDLDotNetCore.NLayer.DataAccess.Services;
+using SSDLDotNetCore.NLayer.DataAccess.Models;
+
+namespace SSDLDotNetCore.RestApiWithNLayer.Features.Blog
+{
+    public class BL_Blog
+    {
+        private readonly DA_Blog _da_Blog;
+
+        public BL_Blog()
+        {
+            _da_Blog = new DA_Blog();
+        }
+
+        public List<BlogModel> GetBlogs()
+        {
+            var lst = _da_Blog.GetBlogs();
+            return lst;
+        }
+
+        public BlogModel GetBlog(int id)
+        {
+            var item = _da_Blog.GetBlog(id);
+            return item;
+        }
+        public int CreateBlog(BlogModel requestModel)
+        {
+            var result = _da_Blog.CreateBlog(requestModel);
+            return result;
+        }
+
+        public int UpdateBlog(int id, BlogModel requestModel)
+        {
+            var result = _da_Blog.UpdateBlog(id, requestModel);
+            return result;
+        }
+
+        public int PatchBlog(int id, BlogModel requestModel)
+        {
+            var result = _da_Blog.PatchBlog(id, requestModel);
+            return result;
+        }
+
+        public int DeleteBlog(int id)
+        {
+            var result = _da_Blog.DeleteBlog(id);
+            return result;
+        }
+    }
+}
---
+++ SSDLDotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SSDLDotNetCore.NLayer.DataAccess.Models;
+using SSDLDotNetCore.RestApiWithNLayer.Features.Blog;
+
+Console.WriteLine("Hello, World!");
+
+BL_Blog bL_Blog = new BL_Blog();
+var model = bL_Blog.GetBlogs();
+
+var lst = model.ToList();
+foreach (BlogModel item in lst)
+{
+    Console.WriteLine(item.BlogId);
+    Console.WriteLine(item.BlogTitle);
+    Console.WriteLine(item.BlogAuthor);
+    Console.WriteLine(item.BlogContent);
+    Console.WriteLine("------------------------------------------");
+}
+
+Console.ReadLine();
