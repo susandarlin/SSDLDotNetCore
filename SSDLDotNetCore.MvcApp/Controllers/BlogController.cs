489ebf2b6908bc60a5857b187bdaf084b6ee39b8
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-06-22 13:37:45 +0700

    MVC - Read and Create

---
+++ SSDLDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SSDLDotNetCore.MvcApp.Db;
+using SSDLDotNetCore.MvcApp.Models;
+
+namespace SSDLDotNetCore.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public BlogController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var lst = await _db.Blogs.ToListAsync();
+            return View(lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> SaveBlog(BlogModel blog)
+        {
+            await _db.Blogs.AddAsync(blog);
+            var result = await _db.SaveChangesAsync();
+            return Redirect("/Blog");
+        }
+    }
+}
---
+++ SSDLDotNetCore.MvcApp/Db/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSDLDotNetCore.MvcApp.Models;
+
+namespace SSDLDotNetCore.MvcApp.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        //}
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
