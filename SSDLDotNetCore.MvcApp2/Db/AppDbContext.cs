9b60c04448a7494ab89f819cf32a6875256f3f36
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-07-15 00:52:27 +0700

    ASP.NET Core MVC Ajax

---
+++ SSDLDotNetCore.MvcApp2/Db/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSDLDotNetCore.MvcApp2.Models;
+
+namespace SSDLDotNetCore.MvcApp2.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+    //}
+    public DbSet<BlogModel> Blogs { get; set; }
+}
