1b2d48fb242c9d79c70e3174faa2f3ca5da46792
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-07-16 23:46:13 +0700

    [Compkete] - ChartJs, HighCharts and CanvasJs Chart

---
+++ SSDLDotNetCore.MvcChartApp/Controllers/ChartJsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSDLDotNetCore.MvcChartApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult ExampleChart()
+        {
+            return View();
+        }
+
+        public IActionResult InterpolationLineChart()
+        {
+            return View();
+        }
+    }
+}
---
+++ SSDLDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSDLDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
