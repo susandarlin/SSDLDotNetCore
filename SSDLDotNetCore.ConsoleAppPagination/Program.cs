e77cf0ba66fa1972f129fdd5cac63ba10318f719
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-08-04 12:36:54 +0700

    Pagination - MVC

--- SSDLDotNetCore.ConsoleAppPagination/Program.cs
+++ SSDLDotNetCore.ConsoleAppPagination/Program.cs
@@ -1,17 +1,17 @@
 ﻿using SSDLDotNetCore.ConsoleAppPagination.Db;
 using SSDLDotNetCore.ConsoleAppPagination.Pagination;
 
-//PaginationExample paginationExample = new PaginationExample();
-//paginationExample.Run();
+PaginationExample paginationExample = new PaginationExample();
+paginationExample.Run();
 
-AppDBContext db = new AppDBContext();
-int pageSize = 10;
+//AppDBContext db = new AppDBContext();
+//int pageSize = 10;
 
-int rowCount = db.Blogs.Count();
+//int rowCount = db.Blogs.Count();
 
-int pageCount = rowCount / pageSize;
-Console.WriteLine("Current Page Count " + pageCount);
+//int pageCount = rowCount / pageSize;
+//Console.WriteLine("Current Page Count " + pageCount);
 
-if(rowCount % pageSize > 0)
-    pageCount++;
-Console.WriteLine("Current Page Count " + pageCount);
+//if(rowCount % pageSize > 0)
+//    pageCount++;
+//Console.WriteLine("Current Page Count " + pageCount);
---
+++ SSDLDotNetCore.MvcPagination/Controllers/BlogPaginationController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SSDLDotNetCore.MvcPagination.Db;
+using SSDLDotNetCore.MvcPagination.Models;
+
+namespace SSDLDotNetCore.MvcPagination.Controllers;
+
+public class BlogPaginationController : Controller
+{
+    private readonly AppDbContext _db;
+
+    public BlogPaginationController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [ActionName("Index")]
+    public IActionResult BlogIndex(int pageNo = 1, int pageSize = 10)
+    {
+        int rowCount = _db.Blogs.Count();
+
+        int pageCount = rowCount / pageSize;
+        if (rowCount % pageSize > 0)
+            pageCount++;
+
+        if (pageNo > pageCount)
+            return Redirect("/Blog");
+
+        List<BlogModel> lst = _db.Blogs
+            //.OrderByDescending(x => x.BlogId)
+            .Skip((pageNo - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        BlogResponseModel model = new();
+        model.Data = lst;
+        model.PageNo = pageNo;
+        model.PageSize = pageSize;
+        model.PageCount = pageCount;
+        //model.IsEndOfPage = pageNo == pageCount;
+
+        return View("BlogIndex", model);
+    }
+}
