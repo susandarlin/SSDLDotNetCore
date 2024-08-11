f2e228b3c76e811a838bf12536742a15c7cb71ba
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-08-11 12:46:49 +0700

    EFCore Database First

---
+++ SSDLDotNetCore.ConsoleAppEFCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using SSDLDotNetCore.ConsoleAppEFCore.Databases.Models;
+
+AppDbContext db = new AppDbContext();
+var lst = db.TblPieCharts.ToList();
+
+for (int i = 0; i < lst.Count; i++)
+{
+    Console.WriteLine(lst[i].PieChartName);
+    Console.WriteLine(lst[i].PieChartValue);
+    Console.WriteLine("-------------------------------");
+}
+
+Console.ReadLine();
