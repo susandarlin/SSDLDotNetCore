2dcaf4e7ecec3e143a7d9dc85e60a7d22802cbed
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-08-15 10:23:57 +0700

    ASP.NET Core MVC UI Design - Admin LTE

---
+++ SSDLDotNetCore.MvcDesignApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSDLDotNetCore.MvcDesignApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
