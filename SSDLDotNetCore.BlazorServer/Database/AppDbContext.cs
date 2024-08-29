1a677b317b8ec34f7e4704ce502f1e7422b219de
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-08-29 23:07:25 +0700

    Blazor Server - Blog List

---
+++ SSDLDotNetCore.BlazorServer/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SSDLDotNetCore.BlazorServer.Models;
+
+namespace SSDLDotNetCore.BlazorServer.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogModel> Blog { get; set; }
+    }    
+}
