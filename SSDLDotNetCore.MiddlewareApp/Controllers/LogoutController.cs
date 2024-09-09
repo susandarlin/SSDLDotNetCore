3bb172640c325fe0df9b5ca0ad175ef3c234f72c
Su Sandar Lin <susandarlin.2016@gmail.com>
2024-09-09 12:10:17 +0700

    Session and Middleware

---
+++ SSDLDotNetCore.MiddlewareApp/Controllers/LogoutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSDLDotNetCore.MiddlewareApp.Controllers
+{
+    public class LogoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/Login");
+        }
+    }
+}
+  
---
+++ SSDLDotNetCore.MiddlewareApp/Middlewares/SessionLoginMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace SSDLDotNetCore.MiddlewareApp.Middlewares
+{
+    public class SessionLoginMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SessionLoginMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(context.Request.Path == "/" || context.Request.Path.ToString().ToLower() == "/Login".ToLower())
+                goto result;
+
+            string name = context.Session.GetString("name")!;
+            if(name is null)
+            {
+                context.Response.Redirect("/");
+                return;
+            }
+
+            result:
+            // Call the next delegate/middleware in the pipeline.
+            await _next(context);
+        }
+    }
+}
