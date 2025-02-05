b683e49632b40330190978e6ded380a0b55124b9
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-02-05 23:50:35 +0700

    feat: add UserModel

--- SSDLDotNetCore.LoginApp/EFDbContext/AppDbContext.cs
+++ SSDLDotNetCore.LoginApp/EFDbContext/AppDbContext.cs
@@ -17,3 +17,13 @@ public class LoginModel
     public DateTime SessionExpired { get; set; }
 }
 
+[Table("Tbl_User")]
+public class UserModel
+{
+    [Key]
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
+
+
