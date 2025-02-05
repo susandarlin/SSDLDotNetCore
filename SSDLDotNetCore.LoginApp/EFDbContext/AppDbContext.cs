ee55d542db00d664920967a0cf70ac4cb0331803
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-02-05 23:48:16 +0700

    feat: add LoginModel

---
+++ SSDLDotNetCore.LoginApp/EFDbContext/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SSDLDotNetCore.LoginApp.EFDbContext;
+
+public class AppDbContext
+{
+}
+
+[Table("Tbl_Login")]
+public class LoginModel
+{
+    [Key]
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string SessionId { get; set; }
+    public DateTime SessionExpired { get; set; }
+}
+
