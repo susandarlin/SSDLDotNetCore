7a75a77d95ac9da3b902ee47bed303f6a6fd05c7
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-02-06 23:57:26 +0700

    feat: add login index method

--- SSDLDotNetCore.LoginApp/Controllers/LoginController.cs
+++ SSDLDotNetCore.LoginApp/Controllers/LoginController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SSDLDotNetCore.LoginApp.EFDbContext;
 
 namespace SSDLDotNetCore.LoginApp.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public LoginController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserModel user)
+        {
+            var item = await _db.Users.FirstOrDefaultAsync(x => x.Email == user.Email && x.Password == user.Password);
+            if (item is null) return View();
+
+            await _db.Logins.AddAsync(new LoginModel
+            {
+                SessionId = Guid.NewGuid().ToString(),
+                UserId = user.Id.ToString(),
+                SessionExpired = DateTime.Now.AddMinutes(30)
+            });
+
+            await _db.SaveChangesAsync();
+
+            return Redirect("/Home");
         }
     }
 }
--- SSDLDotNetCore.LoginApp/EFDbContext/AppDbContext.cs
+++ SSDLDotNetCore.LoginApp/EFDbContext/AppDbContext.cs
@@ -27,8 +27,8 @@ public class LoginModel
 public class UserModel
 {
     [Key]
-    public string UserId { get; set; }
-    public string UserName { get; set; }
+    public int Id { get; set; }
+    public string Email { get; set; }
     public string Password { get; set; }
 }
 
