fc7cd0a0741f4658380b488257cb4259a1c6f0e8
Su Sandar Lin <susandarlin.2016@gmail.com>
2025-02-13 11:48:00 +0700

    feat: generate AppDbContext and Blog Model in command line

--- SSDLDotNetCore.EFCoreDataBaseFirstAuto/Class1.cs
+++ SSDLDotNetCore.EFCoreDataBaseFirstAuto/Class1.cs
@@ -1,7 +1,16 @@
-﻿namespace SSDLDotNetCore.EFCoreDataBaseFirstAuto
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SSDLDotNetCore.EFCoreDataBaseFirstAuto
 {
     public class Class1
     {
         // Scaffold-DbContext "Server= SANDAR\MSSQLSERVER2012; Database=SSDLDotNetCore; User Id=sa; Password=admin123!; TrustServerCertificate = true;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir EfCoreDataModels -Context AppDbContext -Tables Tbl_Blog
+
+
+        // dotnet tool install --global dotnet-ef => this is latest version
+        // dotnet tool uninstall --global dotnet-ef --version 7.0.0
+
+        // dotnet ef dbcontext scaffold "Server= SANDAR\MSSQLSERVER2012; Database=SSDLDotNetCore; User Id=sa; Password=admin123!; TrustServerCertificate = true;" Microsoft.EntityFrameworkCore.SqlServer -o EfCoreDataModels -c AppDbContext -t Tbl_Blog
+
     }
 }
--- SSDLDotNetCore.EFCoreDataBaseFirstAuto/EfCoreDataModels/AppDbContext.cs
+++ SSDLDotNetCore.EFCoreDataBaseFirstAuto/EfCoreDataModels/AppDbContext.cs
@@ -17,6 +17,10 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<TblBlog> TblBlogs { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server= SANDAR\\MSSQLSERVER2012; Database=SSDLDotNetCore; User Id=sa; Password=admin123!; TrustServerCertificate = true;");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<TblBlog>(entity =>
